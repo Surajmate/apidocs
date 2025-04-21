8700ece63ee6a60dfa3249ccb37afe0bd2f3e823
Suraj Mate <smate1@bajajauto.co.in>
2025-04-21 12:47:20 +0530

    changes

--- src/parsePostman.js
+++ src/parsePostman.js
@@ -1,11 +1,8 @@
-// src/parsePostman.js
 export function parsePostmanCollection(collection) {
   const flattened = {};
-
   const recurse = (items) => {
       localStorage.setItem('postman_collection', JSON.stringify(items))
   };
-  
   recurse(collection);
   return flattened;
 }
